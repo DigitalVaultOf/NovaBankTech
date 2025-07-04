05cd643a0bafef167a25a9511e0e77c6f5848dc2
moisescaris <moises.caris@edu.unifil.br>
2025-07-04 12:16:41 -0300

    Fix:Deposito

--- User.Api/DTOS/MovimentationDto.cs
+++ User.Api/DTOS/MovimentationDto.cs
@@ -1,11 +1,14 @@
-﻿using Bank.Api.CamposEnum;
+﻿using System.Text.Json.Serialization;
+using Bank.Api.CamposEnum;
 
 namespace Bank.Api.DTOS
 {
     public class MovimentationDto
     {
+        [JsonIgnore]
         public string? acountNumber { get; set; }
         public decimal value { get; set; }
+        [JsonIgnore]
         public MovimentTypeEnum type { get; set; }
     }
 }
