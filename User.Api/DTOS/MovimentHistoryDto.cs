ac434795ad05431a698b4f8daa81a3b5772653ec
RaulFerna <raulfernandesnetto@gmail.com>
2025-07-04 15:43:06 -0300

    Commit atualizado com codigo incompleto

---
+++ User.Api/DTOS/MovimentHistoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Bank.Api.CamposEnum;
+using User.Api.Model;
+
+namespace Bank.Api.DTOS
+{
+    public class MovimentHistoryDto
+    {
+        public string MovimentTypeEnum { get; set; }
+        public DateTime DateTimeMoviment { get; set; }
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+        public Account AccountFrom { get; set; }
+        public Account AccountTo { get; set; }
+        public Guid Id { get; set; }
+
+    }
+}
---
+++ User.Api/Repositories/Interfaces/IMovementRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bank.Api.Model;
+
+namespace Bank.Api.Repositories.Interfaces
+{
+    public interface IMovementRepository
+    {
+        Task<IEnumerable<Movimentations>>GetMovimentationsAsync(Guid userId);
+
+    }
+}
