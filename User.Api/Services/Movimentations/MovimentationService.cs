0c98938b0d26381f85f367d5d3855bd80cfd602a
moisescaris <moises.caris@edu.unifil.br>
2025-07-07 15:16:49 -0300

    Correção de bugs

--- User.Api/Services/Movimentations/MovimentationService.cs
+++ User.Api/Services/Movimentations/MovimentationService.cs
@@ -18,33 +18,33 @@ namespace Bank.Api.Services.Movimentations
         public async Task<ResponseModel<string>> MovimentationDepositAsync(MovimentationDto data)
         {
             var response = new ResponseModel<string>();
-
-            try
             {
-                var accountNumber = _httpContextAccessor.HttpContext?.User.FindFirst(c => c.Type == "AccountNumber")?.Value;
-                var sql = "EXEC MovimentationsDepositProcedure @p0, @p1, @p2";
-                await _context.Database.ExecuteSqlRawAsync(sql,
-                    data.acountNumber = accountNumber,
-                    data.value,
-                    data.type = 0);
+                try
+                {
+                    var accountNumber = _httpContextAccessor.HttpContext?.User.FindFirst(c => c.Type == "AccountNumber")?.Value;
+                    var sql = "EXEC MovimentationsDepositProcedure @p0, @p1, @p2";
+                    await _context.Database.ExecuteSqlRawAsync(sql,
+                        data.acountNumber = accountNumber,
+                        data.value,
+                        data.type = 0);
 
-                response.Data = "Transferência realizada com sucesso.";
-                return response;
-            }
-            catch (Exception e)
-            {
-                response.Message = $"Erro ao realizar a transferência: {e.Message}";
-                return response;
+                    response.Data = "Transferência realizada com sucesso.";
+                    return response;
+                }
+                catch (Exception e)
+                {
+                    response.Message = $"Erro ao realizar a transferência: {e.Message}";
+                    return response;
+                }
             }
         }
 
         public async Task<ResponseModel<string>> MovimentationWithdrawAsync(MovimentationDto data)
         {
+            var accountNumber = _httpContextAccessor.HttpContext?.User.FindFirst(c => c.Type == "AccountNumber")?.Value;
             var response = new ResponseModel<string>();
-
             try
             {
-                var accountNumber = _httpContextAccessor.HttpContext?.User.FindFirst(c => c.Type == "AccountNumber")?.Value;
                 var sql = "EXEC MovimentationsWithdrawProcedure @p0, @p1, @p2";
                 await _context.Database.ExecuteSqlRawAsync(sql,
                     data.acountNumber = accountNumber,
