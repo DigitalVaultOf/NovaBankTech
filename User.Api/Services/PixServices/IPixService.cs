3b8ae46a3a59bdc33c227c6251b15389f78edd49
moisescaris <moises.caris@edu.unifil.br>
2025-07-10 15:44:56 -0300

    Fix:Bug

---
+++ User.Api/Services/PixServices/IPixService.cs
@@ -0,0 +1,10 @@
+﻿using Bank.Api.DTOS;
+using User.Api.Model;
+
+namespace Bank.Api.Services.PixServices
+{
+    public interface IPixService
+    {
+        Task<ResponseModel<string>> CriarPix(RegistroPixDto data);
+    }
+}
---
+++ User.Api/Services/PixServices/PixService.cs
@@ -0,0 +1,33 @@
+﻿using Bank.Api.DTOS;
+using Bank.Api.Utils;
+using User.Api.Model;
+
+namespace Bank.Api.Services.PixServices
+{
+    public class PixService : IPixService
+    {
+        private readonly PixClient _client;
+
+        public PixService(PixClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ResponseModel<string>> CriarPix(RegistroPixDto data)
+        {
+            var response = new ResponseModel<string>();
+            try
+            {
+                await _client.CriarPixAsync(data);
+                response.Data = "Pix Criado com sucesso";
+                return response;
+
+            }
+            catch (Exception e)
+            {
+                response.Message = $"Não foi possivel criar pix: {e.Message}";
+                return response;
+            }
+        }
+    }
+}
