0c7a7803464ac27862f622ae006cf144e0fade5f
CarlosCimitan <eduardo6cimitan@gmail.com>
2025-07-15 14:45:33 -0300

    add procedures on docker

---
+++ Pix.Api/Data/Scripts/CriarChave.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE CriarChave
+    @IdPix UNIQUEIDENTIFIER,
+    @Name VARCHAR(255),
+    @PixKey VARCHAR(255),
+    @Bank VARCHAR(255),
+    @AccountNumber VARCHAR(255)
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        INSERT INTO Pix (IdPix,NameUser, PixKey, Bank, AccountNumber)
+        VALUES(@IdPix,@Name, @PixKey, @Bank, @AccountNumber );
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+
+        DECLARE @ErroMsg NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR('Erro ao inserir a linha: %s', 16, 1, @ErroMsg);
+    END CATCH
+END;
---
+++ Pix.Api/Data/Scripts/RegistrarTrasnferencia.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE RegistrarTrasnferencia
+    @IdPix UNIQUEIDENTIFIER,
+    @Going VARCHAR(255),
+    @Coming VARCHAR(255),
+    @Amount DECIMAL(18,2)
+AS
+BEGIN
+    IF @Amount < 0
+    BEGIN
+        RAISERROR('O valor da transferência não pode ser negativo.', 16, 1);
+        RETURN;
+    END
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        INSERT INTO TransactionsTable(Id, Going, Coming, Amount)
+        VALUES(@IdPix, @Going, @Coming, @Amount);
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+
+        DECLARE @ErroMsg NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR('Erro ao inserir a linha: %s', 16, 1, @ErroMsg);
+    END CATCH
+END;
