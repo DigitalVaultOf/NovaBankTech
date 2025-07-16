158c970e2bc4efa57c8c703d31e861d8f8c0a6fd
DaniloCossioloDias <danilocossiolodiass@gmail.com>
2025-07-16 14:44:56 -0300

    feat: starta projeto

---
+++ ReportApi/Model/Report.cs
@@ -0,0 +1,10 @@
+﻿namespace ReportApi.Model;
+
+public class Report
+{
+    public string? AcountNumberTo { get; set; }
+    public string MovimentTypeEnum { get; set; }
+    public DateTime DateTimeMoviment { get; set; }
+    public decimal Amount { get; set; }
+    public string? AcountNumber { get; set; }
+}
